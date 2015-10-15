7a5cacebe71dc061e297d620100f56201344ccfd
Mihai Dinca <dincamihai@gmail.com>
2015-10-15 23:00:18 +0300

    simplify script generator

--- liapi/scriptcreator.py
+++ liapi/scriptcreator.py
@@ -5,9 +5,8 @@ from liapi.extractor import Extractor
 class LoadScriptGenerator(object):
 
     def __init__(self, jmx_file_path):
-        extractor = Extractor(jmx_file_path)
-        self.domain = extractor.data['domain']
-        self.script = self.get_script(extractor.data['targets'])
+        self.data = Extractor(jmx_file_path).data
+        self.script = self.get_script()
 
     def _get_batch_item(self, item_data):
         query_string = ''
@@ -20,20 +19,20 @@ class LoadScriptGenerator(object):
             '}}'
         ).format(
             scheme=scheme,
-            domain=self.domain,
+            domain=self.data['domain'],
             method=item_data['method'],
             path=item_data['path'],
             query_string=query_string
         )
 
-    def get_script(self, targets):
+    def get_script(self):
         script_pattern = (
             'http.request_batch({'
                 '{{content}}'
             '})'
         )
         content_items = []
-        for idx, it in enumerate(targets):
+        for idx, it in enumerate(self.data['targets']):
             content_items.append(self._get_batch_item(it))
         content = ','.join(content_items)
         return script_pattern.format(content=content)
