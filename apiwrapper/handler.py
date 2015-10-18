f003ba1103324030264d3f09def3ef5b6bc61d32
Mihai Dinca <dincamihai@gmail.com>
2015-10-18 10:19:39 +0300

    fix script requests payload

--- apiwrapper/handler.py
+++ apiwrapper/handler.py
@@ -2,6 +2,7 @@ import os
 import json
 import loadimpact
 import requests
+import json
 from apiwrapper.scriptcreator import LoadScriptGenerator
 from apiwrapper.extractor import Extractor
 
@@ -14,43 +15,42 @@ class JMXHandler(object):
             self.data = Extractor(jmx_file_path, json.load(config_file)).data
 
     def create_scenario(self):
+        payload = dict(
+            load_script=LoadScriptGenerator(
+                self.data['domain'], self.data['targets']
+            ).script,
+            name="test_scenario"
+        )
         resp = requests.post(
             'https://api.loadimpact.com/v2/user-scenarios',
-            data=dict(
-                load_script=LoadScriptGenerator(
-                    self.data['domain'], self.data['targets']
-                ).script,
-                name="test_scenario"
-            ),
-            headers={
-                "Content-Type": "application/json"
-            },
+            data=json.dumps(payload),
+            headers={"Content-Type": "application/json"},
             auth=(self.token, '')
         )
         resp.raise_for_status()
         return resp.json()['id']
 
     def create_test_config(self, scenario_id):
+        payload = {
+            'name': self.data['test_plan_name'],
+            'url': 'http://%s/' % self.data['domain'],
+            'config': {
+                "user_type": "sbu",
+                "load_schedule": [{"users": 10, "duration": 10}],
+                "tracks": [{
+                    "clips": [{
+                        "user_scenario_id": scenario_id,
+                        "percent": 100
+                    }],
+                    "loadzone": loadimpact.LoadZone.AMAZON_US_ASHBURN
+                }]
+            }
+        }
         resp = requests.post(
             url='https://api.loadimpact.com/v2/test-configs',
-            data={
-                'name': self.data['test_plan_name'],
-                'url': 'http://%s/' % self.data['domain'],
-                'config': {
-                    "user_type": "sbu",
-                    "load_schedule": [{"users": 10, "duration": 10}],
-                    "tracks": [{
-                        "clips": [{
-                            "user_scenario_id": scenario_id,
-                            "percent": 100
-                        }],
-                        "loadzone": loadimpact.LoadZone.AMAZON_US_ASHBURN
-                    }]
-                }
-            },
-            headers={
-                "Content-Type": "application/json"
-            },
+            data=json.dumps(payload),
+            headers={"Content-Type": "application/json"},
             auth=(self.token, '')
         )
+        resp.raise_for_status()
         return resp.json()['id']
